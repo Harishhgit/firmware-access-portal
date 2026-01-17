21ffd74e5800fe0f7a4dad5834ecf69016b1e938
rtulgit <rtulgithub@gmail.com>
2026-01-17 17:58:42 +0530

    added new lines for debugging purpose

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -8,11 +9,17 @@ import authRoutes from "./routes/auth.routes.js";
 import testRoutes from "./routes/test.routes.js";
 
 const app = express();
+app.use(cors()); // This tells the browser: "I accept requests from Hoppscotch!"
 app.use(express.json());
 
 console.log("🔥 App starting...");
 
 await connectDB();
+
+app.use((req, res, next) => {
+  console.log(`📡 Incoming Request: ${req.method} ${req.url}`);
+  next();
+});
 
 app.use("/auth", authRoutes);
 app.use("/test", testRoutes);
